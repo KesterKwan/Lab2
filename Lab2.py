67f6cc34c12b78d2a018cdd71eddcc00e989ee95
Kester <kwankester.24@ichat.sp.edu.sg>
2025-05-02 17:25:32 +0800

    updated Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,70 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+import statistics
+temp=[]
+time=True
+minmax=[]
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input(temp):
+    time=True
+   
+    while time:
+        tempinput=float(input("What is the temp? "))
+        temp.append(tempinput)
+        
+        con=input("Do you wish to continue(Y/N)? ")
+        if con == 'Y':
+            time=True
+        else:
+            time=False
+
+def calc_average():
+    print("calc_average")
+    xs=sum(temp)
+    x=len(temp)
+    avg=xs/x
+    print("The average is", avg)
+
+def find_min_max():
+    minmax.append(min(temp))
+    minmax.append(max(temp))
+    print("The lowest and highest temp are:",minmax)
+
+                  
+def sort_temperature():
+    temp.sort()
+    print("Accending order is:",temp)
+
+def calc_median_temperature():
+    statistics.median(temp)
+    print("The median is",statistics.median(temp))
+
+    
+
+        
+
+
+        
+
+       
+        
 
 
 
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input(temp)
+    print(temp)
+    calc_average()
+    find_min_max()
+    sort_temperature()
+    calc_median_temperature()
+    
+
+
+
+if __name__ == "__main__":
+    main()
--- bmi.py
+++ bmi.py
@@ -1,9 +1,12 @@
 def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
+    print("Height = " , height)
+    print("Weight = " , weight)
 
     bmi=round(weight/(height * height),1)
     print("Bmi is",bmi)
+    return bmi
+
+def check_bmi(bmi):   
     if bmi < 18.5:
         print("Under Weight")
     elif bmi >= 18.5 and bmi <= 25.0:
@@ -12,6 +15,14 @@ def calculate_bmi(height, weight):
         print("Over Weight")
 
 
+def main():
+
+    height=float(input("What is your weight (in kg)? "))
+    weight=float(input("What is your height (in metres)? "))
+    calculate_bmi(weight ,height)
+    check_bmi(bmi)
 
 
-calculate_bmi(weight=57, height=1.73)
+
+
+if __name__ == "__main__":main()
